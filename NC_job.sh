9f98294c41d2ec63385e49896c3b032a6432d8dc
apollo994 <fabio.zanarell0.94@gmail.com>
2019-06-10 16:32:39 +0100

    no more result folder

--- NC_job.sh
+++ NC_job.sh
@@ -10,27 +10,19 @@ input=$2
 output=$3
 speed=$4
 
-res_folder=${job_name}_res
-
-mkdir -p ${res_folder}
 
 
 if [ "$speed" == "fast" ];then
 	./NCBoost_scripts/ncboost_annotate.sh \
-	${input} \
-	${res_folder}/${output}
+	$input \
+	$output
 else
 	bsub -G team151 \
-	-o /$res_folder/${job_name}.out \
+	-o ${job_name}.out \
 	-M 4000 \
 	-J $job_name \
 	-R"select[mem>=4000] rusage[mem=4000] span[hosts=1]" \
 	-n1 \
 	-q normal \
-	-- "./NCBoost_scripts/ncboost_annotate.sh $input $res_folder/$output "
+	-- "./NCBoost_scripts/ncboost_annotate.sh $input $output "
 fi
-
-
-#./NCBoost_scripts/ncboost_annotate.sh \
-#${input} \
-#${res_folder}/${output}
--- my_NC_job.sh
+++ my_NC_job.sh
@@ -10,28 +10,19 @@ input=$2
 output=$3
 speed=$4
 
-res_folder=${job_name}_res
-
-mkdir -p ${res_folder}
-
 
 if [ "$speed" == "fast" ];then
 	./my_NCBoost_scripts/ncboost_annotate.sh \
-	${input} \
-	${res_folder}/${output} \
+	$input \
+	$output \
 	5
 else
 	bsub -G team151 \
-	-o /$res_folder/${job_name}.out \
+	-o ${job_name}.out \
 	-M 4000 \
 	-J $job_name \
 	-R"select[mem>=4000] rusage[mem=4000] span[hosts=1]" \
 	-n1 \
 	-q normal \
-	-- "./my_NCBoost_scripts/ncboost_annotate.sh $input $res_folder/$output 5 "
+	-- "./my_NCBoost_scripts/ncboost_annotate.sh $input $output 5 "
 fi
-
-
-#./NCBoost_scripts/ncboost_annotate.sh \
-#${input} \
-#${res_folder}/${output}
