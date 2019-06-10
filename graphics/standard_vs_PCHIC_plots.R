670efa05f0274523e298525afab1351b0fcdaafb
apollo994 <fabio.zanarell0.94@gmail.com>
2019-06-10 19:52:33 +0100

    boxplot to be finish

---
+++ graphics/standard_vs_PCHIC_plots.R
@@ -0,0 +1,59 @@
+#script to compare standard and PCICH score values of intergenic position
+
+library("ggplot2")
+library("gridExtra")
+library("grid")
+library("RColorBrewer")
+library("Hmisc")
+
+
+###############################################################################################
+
+#data
+
+data<-read.table("../../PCHIC_exp/standard_PCICH_comparison/standard_PCHIC_merge.txt", sep = "\t", header = T, row.names = 1)
+data[data=='NA'] <- "NA"
+colnames(data)<-c("ANN_gene","standard_NCscore","PCHIC_gene","PCICH_NCscore")
+
+data[5]<-NULL
+
+res_file="standard_vs_PCHIC_plots.pdf"
+
+#added columns
+
+#columns to see differences between standard and PCICH value
+data$score_change<-ifelse(data$standard_NCscore<data$PCICH_NCscore,"UP",ifelse(data$standard_NCscore==data$PCICH_NCscore, "EQ", "DW"))
+
+#column to see if the gene name changes
+data$gene_change<-ifelse(as.character(data$ANN_gene)!=as.character(data$PCHIC_gene),"New","Same")
+
+#column to see how many new genes has been recovered
+data$recovered<-ifelse(data$ANN_gene=="NOT_FOUND" & data$PCHIC_gene!="NOT_FOUND","1","0")
+
+
+
+###############################################################################################
+
+#graphics
+
+#scatterplot precomp vs new
+
+p<-ggplot(data, aes(x=data$standard_NCscore, y=data$PCICH_NCscore))+geom_point()#+
+ #stat_summary(fun.data=mean_cl_normal) + geom_smooth(method='glm')
+p<-p + labs(title = "Standard vs new assignation NCscore",
+            x = "Standard score", y= "PCHIC score")
+
+#barplot of the score value column
+a<-ggplot(data, aes(x=data$score_change))+geom_bar(aes(fill=gene_change))
+a<-a + scale_fill_brewer(palette="Set1")
+a<-a + annotate("text", x=1, y=nrow(na.omit(data[data$score_change=="DW",]))+150, label = nrow(na.omit(data[data$score_change=="DW",])) , color="black", size=5 )
+a<-a + annotate("text", x=2, y=nrow(na.omit(data[data$score_change=="EQ",]))+150, label = nrow(na.omit(data[data$score_change=="EQ",])) , color="black", size=5 )
+a<-a + annotate("text", x=3, y=nrow(na.omit(data[data$score_change=="UP",]))+150, label = nrow(na.omit(data[data$score_change=="UP",])) , color="black", size=5 )
+a<-a + annotate("text", x=4, y=nrow(data[is.na(data$score_change),])+150, label = nrow(data[is.na(data$score_change),]) , color="black", size=5 )
+a<-a + labs(title = "PCHIC gene reassignation result ", x= "Direction of SCORE change")
+
+
+
+#da finire distribuzione di NC e PC score nei new gene
+e<-ggplot(data[data$gene_change=="New",], aes(x=data$gene_change[data$gene_change=="New"], y=data$PCICH_NCscore[data$gene_change=="New",])) + geom_boxplot()
+
